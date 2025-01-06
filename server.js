68dbd55069589b2236f1e6c3cb984f23074a45ab
Pradeep-React-Node <dev.react@vibgyorweb.com>
2025-01-06 22:50:35 +0530

    na

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ app.use(cors());
 const MERCHANT_KEY="099eb0cd-02cf-4e2a-8aca-3e6c6aff0399"
 const MERCHANT_ID="PGTESTPAYUAT"
 
-const MERCHANT_BASE_URL = "https://api.phonepe.com/apis/hermes/pg/v1/pay"
-const MERCHANT_STATUS_URL = "https://api.phonepe.com/apis/hermes/pg/v1/status"
+// const MERCHANT_BASE_URL = "https://api.phonepe.com/apis/hermes/pg/v1/pay"
+// const MERCHANT_STATUS_URL = "https://api.phonepe.com/apis/hermes/pg/v1/status"
 
-// const MERCHANT_BASE_URL="https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay"
-// const MERCHANT_STATUS_URL="https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/status"
+const MERCHANT_BASE_URL="https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay"
+const MERCHANT_STATUS_URL="https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/status"
 
 const redirectUrl="https://phonepe-kek2.onrender.com/status"
 
